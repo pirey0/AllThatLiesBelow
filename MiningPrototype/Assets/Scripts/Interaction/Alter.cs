15a57ba7a8ba1333ca0ee7e5c4ef118036839b4f
Luca Martinelli <lucxmangajet@gmail.com>
2020-11-10 15:58:23 +0100

    moved scripts, added alter base

---
+++ MiningPrototype/Assets/Scripts/Interaction/Alter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alter : MonoBehaviour, IInteractable
+{
+    AltarDialogVisualizer visualizer;
+
+    public void BeginInteracting(GameObject interactor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EndInteracting(GameObject interactor)
+    {
+        throw new System.NotImplementedException();
+    }
+}
