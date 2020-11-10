670181e2a3c625fa92f9cb1802c3f9c57c2d48e9
Luca Martinelli <lucxmangajet@gmail.com>
2020-11-10 20:01:39 +0100

    added dialog test

--- MiningPrototype/Assets/Scripts/Dialog/Dialog.cs
+++ MiningPrototype/Assets/Scripts/Dialog/Dialog.cs
@@ -2,124 +2,127 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Statement : IDialogSubsection
+namespace DialogTest2
 {
-    string message;
-    IDialogSubsection connection;
-    public Statement(string message)
+    public class Statement : IDialogSubsection
     {
-        this.message = message;
-    }
-
-    public void Enter()
-    {
-        //Send message 
-    }
-
-    public IDialogSubsection GetConnectedSubsection()
-    {
-        return connection;
-    }
-
-    public bool TryExit(int parameter)
-    {
-        return true;
-    }
-
-    public IDialogSubsection Attach(IDialogSubsection subsection)
-    {
-        connection = subsection;
-        return this;
-    }
-}
-
-public class Choice : IDialogSubsection
-{
-    List<ChoicePair> choices;
-    IDialogSubsection connection;
-
-    public Choice(params ChoicePair[] choicesParams)
-    {
-        choices = new List<ChoicePair>(choicesParams);
-    }
-
-    public void AddChoice(string option, IDialogSubsection subsection)
-    {
-        choices.Add(new ChoicePair(option, subsection));
-    }
-
-    public void Enter()
-    {
-        //Send choice
-    }
-
-    public IDialogSubsection GetConnectedSubsection()
-    {
-        return connection;
-    }
-
-    public bool TryExit(int parameter)
-    {
-        if(parameter >= 0 && parameter < choices.Count)
+        string message;
+        IDialogSubsection connection;
+        public Statement(string message)
         {
-            connection = choices[parameter].Subsection;
+            this.message = message;
+        }
+
+        public void Enter()
+        {
+            //Send message 
+        }
+
+        public IDialogSubsection GetConnectedSubsection()
+        {
+            return connection;
+        }
+
+        public bool TryExit(int parameter)
+        {
             return true;
         }
-        return false;
-    }
-}
 
-public class AwaitPayment : IDialogSubsection
-{
-    IDialogSubsection connection;
-    IDialogSubsection successful;
-    IDialogSubsection failure;
-    public void Enter()
+        public IDialogSubsection Attach(IDialogSubsection subsection)
+        {
+            connection = subsection;
+            return this;
+        }
+    }
+
+    public class Choice : IDialogSubsection
     {
-        //Notify payment await
+        List<ChoicePair> choices;
+        IDialogSubsection connection;
+
+        public Choice(params ChoicePair[] choicesParams)
+        {
+            choices = new List<ChoicePair>(choicesParams);
+        }
+
+        public void AddChoice(string option, IDialogSubsection subsection)
+        {
+            choices.Add(new ChoicePair(option, subsection));
+        }
+
+        public void Enter()
+        {
+            //Send choice
+        }
+
+        public IDialogSubsection GetConnectedSubsection()
+        {
+            return connection;
+        }
+
+        public bool TryExit(int parameter)
+        {
+            if (parameter >= 0 && parameter < choices.Count)
+            {
+                connection = choices[parameter].Subsection;
+                return true;
+            }
+            return false;
+        }
     }
 
-    public IDialogSubsection GetConnectedSubsection()
+    public class AwaitPayment : IDialogSubsection
     {
-        return connection;
+        IDialogSubsection connection;
+        IDialogSubsection successful;
+        IDialogSubsection failure;
+        public void Enter()
+        {
+            //Notify payment await
+        }
+
+        public IDialogSubsection GetConnectedSubsection()
+        {
+            return connection;
+        }
+
+        public bool TryExit(int parameter)
+        {
+
+            return true;
+        }
+
+        public AwaitPayment SucceedsIn(IDialogSubsection subsection)
+        {
+            successful = subsection;
+            return this;
+        }
+
+        public AwaitPayment FailsIn(IDialogSubsection subsection)
+        {
+            failure = subsection;
+            return this;
+        }
     }
 
-    public bool TryExit(int parameter)
+
+    public class ChoicePair
     {
+        public string Option;
+        public IDialogSubsection Subsection;
 
-        return true;   
+        public ChoicePair(string option, IDialogSubsection subsection)
+        {
+            Option = option;
+            Subsection = subsection;
+        }
     }
 
-    public AwaitPayment SucceedsIn(IDialogSubsection subsection)
+    public interface IDialogSubsection
     {
-        successful = subsection;
-        return this;
+        void Enter();
+
+        bool TryExit(int parameter);
+        IDialogSubsection GetConnectedSubsection();
     }
-
-    public AwaitPayment FailsIn(IDialogSubsection subsection)
-    {
-        failure = subsection;
-        return this;
-    }
-}
-
-
-public class ChoicePair
-{
-    public string Option;
-    public IDialogSubsection Subsection;
-
-    public ChoicePair(string option, IDialogSubsection subsection)
-    {
-        Option = option;
-        Subsection = subsection;
-    }
-}
-
-public interface IDialogSubsection
-{
-    void Enter();
-
-    bool TryExit(int parameter);
-    IDialogSubsection GetConnectedSubsection();
 }
--- MiningPrototype/Assets/Scripts/Dialog/DialogHandler.cs
+++ MiningPrototype/Assets/Scripts/Dialog/DialogHandler.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogHandler : Singleton<DialogHandler>
+namespace DialogTest2
 {
-    
-    public IDialogSubsection GetPaymentProcess()
+
+
+    public class DialogHandler : Singleton<DialogHandler>
     {
-        Statement current = new Statement("and how will you pay?");
-        Statement success = new Statement("in the morning you will receive your reward");
-        Statement failure = new Statement("I will await..");
 
-        current.Attach
-            (new Choice(
-                new ChoicePair("Rocks", new Statement("Does it look like i need more rocks?").Attach(current)),
-                new ChoicePair("Copper", new Statement("20 Copper will suffice").Attach(new AwaitPayment().SucceedsIn(success).FailsIn(failure))),
-                new ChoicePair("Gold", new Statement("5 Gold is the price").Attach(new AwaitPayment().SucceedsIn(success).FailsIn(failure))
-                )));
 
-        return current;
+
+        public IDialogSubsection GetPaymentProcess()
+        {
+            Statement current = new Statement("and how will you pay?");
+            Statement success = new Statement("in the morning you will receive your reward");
+            Statement failure = new Statement("I will await..");
+
+            current.Attach
+                (new Choice(
+                    new ChoicePair("Rocks", new Statement("Does it look like i need more rocks?").Attach(current)),
+                    new ChoicePair("Copper", new Statement("20 Copper will suffice").Attach(new AwaitPayment().SucceedsIn(success).FailsIn(failure))),
+                    new ChoicePair("Gold", new Statement("5 Gold is the price").Attach(new AwaitPayment().SucceedsIn(success).FailsIn(failure))
+                    )));
+
+            return current;
+        }
+
     }
-
 }
