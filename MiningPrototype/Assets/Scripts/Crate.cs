60b788182b691e20cfc724a78eb1c560b4585cac
LucasBoe <lucas@lumotex.de>
2020-11-13 12:53:37 +0100

    added propper destruction behaviour to crate

--- MiningPrototype/Assets/Scripts/Crate.cs
+++ MiningPrototype/Assets/Scripts/Crate.cs
@@ -1,29 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Crate : MonoBehaviour, IMinableNonGrid
 {
     [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] PassiveSpriteAnimator overlayAnimator;
+    [SerializeField] float damageMultiplier = 1;
+
+    [SerializeField] ItemAmountPair contains;
+    [SerializeField] GameObject destroyEffects;
+
+    float damage = 0;
+
     public void Damage(float v)
     {
-        spriteRenderer.color = Color.red;
+        float d = v * (damageMultiplier/10f);
+        Debug.LogWarning("damage: " + d + " (" + damage + ")");
+        overlayAnimator.ActiveUpdate(d);
+        damage += d;
+
+        if (damage >= 1)
+            Destroyed();
+    }
+
+    private void Destroyed()
+    {
+        if (contains != null && contains.amount > 0)
+            InventoryManager.PlayerCollects(contains.type, contains.amount);
+
+        Instantiate(destroyEffects, GetPosition(), Quaternion.identity);
+
+        Destroy(gameObject);
     }
 
     public Vector2 GetPosition()
     {
-        return transform.position;
+        return transform.position + (spriteRenderer.size.y/2) * Vector3.up;
     }
 
     public void MouseEnter()
     {
-        spriteRenderer.color = Color.yellow;
-        Debug.LogWarning("enter");
+        //
     }
 
     public void MouseLeave()
     {
-        spriteRenderer.color = Color.white;
-        Debug.LogWarning("leave");
+        //
     }
 }
---
+++ MiningPrototype/Assets/Scripts/SpriteAnimator/PassiveSpriteAnimator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveSpriteAnimator : MonoBehaviour
+{
+    [SerializeField] SpriteAnimation startingAnimation;
+
+    [SerializeField] new SpriteRenderer renderer;
+
+    BasicSpriteAnimator basicSA = new BasicSpriteAnimator();
+
+    public SpriteAnimation Animation { get => basicSA.Animation; }
+    public SpriteRenderer Renderer { get => renderer; }
+    private void Start()
+    {
+
+        if (renderer == null)
+        {
+            renderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        if (startingAnimation != null)
+        {
+            Play(startingAnimation);
+        }
+    }
+
+    public void Play(SpriteAnimation ani, bool resetSame = true)
+    {
+        basicSA.Play(ani, resetSame);
+    }
+
+    public bool IsDone()
+    {
+        return basicSA.IsDone();
+    }
+
+    public void ActiveUpdate(float updateTime)
+    {
+        if (renderer == null)
+            return;
+
+        var sprite = basicSA.Update(updateTime);
+
+        if (sprite != null)
+            renderer.sprite = sprite;
+    }
+}
