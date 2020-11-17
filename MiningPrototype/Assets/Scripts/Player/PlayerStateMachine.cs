0bfe0de60eca0dd4b4bae168ce6c862951db426e
Luca Martinelli <lucxmangajet@gmail.com>
2020-11-17 19:47:47 +0100

    disabled player states buttons for build

--- MiningPrototype/Assets/Scripts/Player/PlayerStateMachine.cs
+++ MiningPrototype/Assets/Scripts/Player/PlayerStateMachine.cs
@@ -52,6 +52,7 @@ public class PlayerStateMachine : MonoBehaviour, IStateMachineUser, IEntity
     [SerializeField] bool slowWalkMode;
     [SerializeField] StringBoolPair[] statesCanInteract;
     [SerializeField] PlayerInteractionHandler playerInteraction;
+    [SerializeField] bool debug;
 
     StateMachine stateMachine;
     StateMachine.State s_idle, s_jump, s_fall, s_walk, s_slowWalk, s_climb, s_climbIde, s_inventory, s_death, s_hit, s_longIdle, s_disabled;
@@ -95,6 +96,9 @@ public class PlayerStateMachine : MonoBehaviour, IStateMachineUser, IEntity
 
     private void OnGUI()
     {
+        if (!debug)
+            return;
+
         GUI.color = Color.black;
         GUI.Label(new Rect(10, 10, 200, 25), stateMachine.CurrentState.Name);
 
