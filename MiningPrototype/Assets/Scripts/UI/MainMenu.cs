fbddb120a5f60935b5b7e8522549ac6b284a03a3
LucasBoe <lucas@lumotex.de>
2020-11-17 18:23:38 +0100

    added some functionality to main menu

---
+++ MiningPrototype/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    public void MenueAction(string coroutineName)
+    {
+        StartCoroutine(coroutineName);
+    }
+
+    private IEnumerator Play()
+    {
+        animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    private IEnumerator Load()
+    {
+        yield return null;
+    }
+
+    private IEnumerator Settings()
+    {
+        yield return null;
+    }
+
+    private IEnumerator Quit()
+    {
+        animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(0.5f);
+        Application.Quit();
+    }
+}
