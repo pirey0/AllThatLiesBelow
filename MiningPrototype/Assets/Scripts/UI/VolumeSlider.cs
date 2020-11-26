8a2894d4807585feb2d1f7366c1c4a717e4ee64a
Luca Martinelli <lucxmangajet@gmail.com>
2020-11-26 22:45:22 +0100

    volume slider now follows logarithmic sound rules

--- MiningPrototype/Assets/Scripts/UI/VolumeSlider.cs
+++ MiningPrototype/Assets/Scripts/UI/VolumeSlider.cs
@@ -13,14 +13,26 @@ public class VolumeSlider : MonoBehaviour
     {
         if (mixer.GetFloat("MasterVolume", out float vol))
         {
-            slider.value = vol;
+            slider.value = DBTo01(vol);
         }
     }
 
-    public void SetVolume(float db)
+    public void SetVolume(float s01)
     {
-        mixer.SetFloat("MasterVolume", db);
+        mixer.SetFloat("MasterVolume", SliderToDB(s01));
     }
 
+    // See https://sound.stackexchange.com/questions/38722/convert-db-value-to-linear-scale
+    float DBTo01(float db)
+    {
+        return Mathf.Pow(10, db / 20);
+    }
 
+    float SliderToDB(float v01)
+    {
+        if (v01 == 0) //avoid Log(0) =1
+            return -80;
+
+        return 20 * Mathf.Log10(v01);
+    }
 }
