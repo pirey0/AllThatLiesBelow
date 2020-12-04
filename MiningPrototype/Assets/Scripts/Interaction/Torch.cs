5b24f7e358bfa6567d8dce7c6949562b1684a012
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-05 00:41:54 +0100

    torch reach decreased over depth

--- MiningPrototype/Assets/Scripts/Interaction/Torch.cs
+++ MiningPrototype/Assets/Scripts/Interaction/Torch.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Torch : MineableObject
-{    
+{
+    [SerializeField] float z = 0.9f;
+    [SerializeField] AnimationCurve innerRadiusOverY, outerRadiusOverY;
+
     private void Start ()
     {
-        //torches are in the background (0.9f)
-        transform.position = new Vector3(transform.position.x, transform.position.y, 0.9f);
+        //torches are in the background (z)
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+
+        var pl = GetComponent<Light2D>();
+
+        float y = transform.position.y;
+        float ir = innerRadiusOverY.Evaluate(y);
+        float or = outerRadiusOverY.Evaluate(y);
+        pl.pointLightInnerRadius = ir;
+        pl.pointLightOuterRadius = or;
     }
 }
