e40540ba8f9e59421f266b4d98bc1f5804a30021
LucasBoe <lucas@lumotex.de>
2020-12-10 13:47:47 +0100

    added first draft of intro and text display

---
+++ MiningPrototype/Assets/Scripts/Intro.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public List<IntroTextElement> texts = new List<IntroTextElement>();
+    [SerializeField] DialogElementVisualization textPrefab;
+    [SerializeField] Canvas textCanvas;
+    [SerializeField] RectTransform textSpawnPosition;
+
+    private void Start()
+    {
+        foreach (IntroTextElement element in texts)
+        {
+            StartCoroutine(DisplayTextDelayed(element));
+        }
+    }
+
+    IEnumerator DisplayTextDelayed(IntroTextElement element)
+    {
+        yield return new WaitForSeconds(element.time);
+        DialogElementVisualization text = Instantiate(textPrefab, textSpawnPosition.position, Quaternion.identity, textCanvas.transform);
+        text.Init(null,element.text,element.duration);
+    }
+}
+
+[System.Serializable]
+public class IntroTextElement
+{
+    public float time;
+    public string text;
+    public float duration;
+}
--- MiningPrototype/Assets/Scripts/Player/MainMenuPlayerStateMachine.cs
+++ MiningPrototype/Assets/Scripts/Player/MainMenuPlayerStateMachine.cs
@@ -6,6 +6,7 @@ public class MainMenuPlayerStateMachine : BasePlayerStateMachine
 {
     [SerializeField] float moveX;
     [SerializeField] float moveY;
+    [SerializeField] bool inPlace;
     Vector3 position;
     protected override float GetHorizontalInput()
     {
@@ -20,9 +21,12 @@ public class MainMenuPlayerStateMachine : BasePlayerStateMachine
     //hold the player in place to prevent him from falling down
     private void Update()
     {
-        if (position != Vector3.zero)
-            transform.position = position;
+        if (inPlace)
+        {
+            if (position != Vector3.zero)
+                transform.position = position;
 
-        position = transform.position;
+            position = transform.position;
+        }
     }
 }
