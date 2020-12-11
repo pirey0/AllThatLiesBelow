99f64083aa9cc84c63123c008c7aeb03d203ee91
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-11 17:52:11 +0100

    added loading ball

---
+++ MiningPrototype/Assets/Scripts/UI/LoadingDisplayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingDisplayer : StateListenerBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] float rotSpeed;
+
+    private void Update()
+    {
+        image.transform.Rotate(0, 0, Time.unscaledDeltaTime * rotSpeed);
+    }
+
+    protected override void OnStartAfterLoad()
+    {
+        Destroy(gameObject);
+    }
+}
