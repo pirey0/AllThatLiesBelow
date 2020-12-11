ece062ba2d92fd2b677772072848fe198bdcff4e
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-11 17:30:15 +0100

    location indicator is now savable

--- MiningPrototype/Assets/Scripts/Player/LocationIndicator.cs
+++ MiningPrototype/Assets/Scripts/Player/LocationIndicator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class LocationIndicator : MonoBehaviour
+public class LocationIndicator : MonoBehaviour, INonPersistantSavable
 {
     public IndicatorType Type;
 
@@ -17,7 +17,30 @@ public class LocationIndicator : MonoBehaviour
     {
         return GameObject.FindObjectsOfType<LocationIndicator>().First((x) => x.Type == type);
     }
-    
+
+    public SpawnableSaveData ToSaveData()
+    {
+        var data = new LocationIndicatorSaveData();
+        data.Type = Type;
+        data.Position = new SerializedVector3(transform.position);
+        data.Rotation = new SerializedVector3(transform.eulerAngles);
+        data.SpawnableIDType = SpawnableIDType.LocationIndicator;
+        return data;
+    }
+
+    public void Load(SpawnableSaveData sdata)
+    {
+        if(sdata is LocationIndicatorSaveData data)
+        {
+            Type = data.Type;
+        }
+    }
+
+    [System.Serializable]
+    public class LocationIndicatorSaveData : SpawnableSaveData
+    {
+        public IndicatorType Type;
+    }
 }
 public enum IndicatorType
 {
