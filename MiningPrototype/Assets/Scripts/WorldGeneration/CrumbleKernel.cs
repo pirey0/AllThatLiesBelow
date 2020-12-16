85d301c0cbdd7751d1507ff3563cf487b76536c1
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-16 21:45:39 +0100

    added base for kernels

---
+++ MiningPrototype/Assets/Scripts/WorldGeneration/CrumbleKernel.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Flags]
+public enum CrumbleType
+{
+    Null = 0,
+    Crumble = 1,
+    Air = 2,
+    Stone = 4,
+    Ignore = 8,
+
+}
+
+
+[System.Serializable]
+public class Kernel
+{
+    CrumbleType[,] map;
+    int width, height;
+
+    public int Width { get => width; }
+    public int Height { get => height; }
+    public CrumbleType this[int x, int y] { get => map[x, y]; set => TrySet(x, y, value); }
+
+    public void TrySet(int x, int y, CrumbleType type)
+    {
+        if(x<0 || y>= width ||y <0 || y >= height)
+        {
+            return;
+        }
+
+        map[x, y] = type;
+    }
+
+    public Kernel(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        map = new CrumbleType[width, height];
+    }
+
+    //Constructor from string Array meant for parsing text file with kernels
+    public static Kernel FromStrings(string[] str)
+    {
+        if (str.Length == 0)
+            return null;
+
+        Kernel k = new Kernel(str[0].Length, str.Length);
+
+
+        for (int y = 0; y < k.Height; y++)
+        {
+            for (int x = 0; x < k.Width; x++)
+            {
+                k[x, y] = CharToCrumbleType(str[y][x]);
+            }
+        }
+
+        return k;
+    }
+
+
+    public static CrumbleType CharToCrumbleType(char c)
+    {
+        
+        switch (c)
+        {
+            case '0':
+                return CrumbleType.Air;
+            case '1':
+                return CrumbleType.Stone;
+            case 'I':
+            case 'i':
+                return CrumbleType.Ignore;
+            case 'c':
+            case 'C':
+                return CrumbleType.Crumble;
+
+            default:
+                Debug.LogError("Found unknown character: " + c + " " + (int)c);
+                return CrumbleType.Null;
+        }
+    }
+
+}
---
+++ MiningPrototype/Assets/Scripts/WorldGeneration/KernelParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KernelParser : MonoBehaviour
+{
+    [SerializeField] TextAsset[] kernelFiles;
+
+    Kernel[] kernels;
+
+    private void Awake()
+    {
+        LoadKernels();
+    }
+
+    private void LoadKernels()
+    {
+        List<Kernel> kernels = new List<Kernel>();
+        foreach (var k in kernelFiles)
+        {
+            AddKernelsFrom(k, kernels);
+        }
+
+        this.kernels = kernels.ToArray();
+    }
+
+    private void AddKernelsFrom(TextAsset k, List<Kernel> kernels)
+    {
+        string[] lines = k.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        List<string> currentLines = new List<string>();
+        int index = 0;
+
+        while (index < lines.Length)
+        {
+            string currentLine = lines[index].Replace(" ",""); //Remove space character
+            if (currentLine.StartsWith("//"))
+            {
+                //do nothing on comment
+            }
+            else if (string.IsNullOrWhiteSpace(currentLine))
+            {
+                if (currentLines.Count > 0)
+                {
+                    kernels.Add(Kernel.FromStrings(currentLines.ToArray()));
+                    currentLines.Clear();
+                }
+            }
+            else
+            {
+                currentLines.Add(currentLine);
+            }
+            index++;
+        }
+    }
+}
