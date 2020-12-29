60130ac782ec5d05b8bfb42b99d6a74ce1c04c6a
Luca Martinelli <lucxmangajet@gmail.com>
2020-12-29 20:13:54 +0100

    added performance tester to test at different FPS

---
+++ MiningPrototype/Assets/Scripts/PerformanceTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerformanceTester : MonoBehaviour
+{
+    [NaughtyAttributes.OnValueChanged("OnTargetChanged")]
+    [SerializeField] int Framerate;
+
+
+    private void Awake()
+    {
+        if (Framerate > 0)
+            OnTargetChanged();
+    }
+
+    private  void OnTargetChanged()
+    {
+        if (!Application.isPlaying)
+            return;
+
+        Application.targetFrameRate = Framerate;
+        QualitySettings.vSyncCount = 0;
+        Debug.Log("New Target Framerate: " + Application.targetFrameRate );
+    }
+
+}
