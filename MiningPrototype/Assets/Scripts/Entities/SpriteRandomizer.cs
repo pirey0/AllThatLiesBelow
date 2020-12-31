e7741e07b65f2a821d170093325fc2e5ba227c9f
LucasBoe <lucas@lumotex.de>
2020-12-31 14:01:34 +0100

    skeletons dont have outlines anymore and are now randomised visually

---
+++ MiningPrototype/Assets/Scripts/Entities/SpriteRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRandomizer : MonoBehaviour
+{
+    [SerializeField] Sprite[] sprites;
+    private void OnEnable()
+    {
+        if (sprites.Length > 0)
+            GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+}
