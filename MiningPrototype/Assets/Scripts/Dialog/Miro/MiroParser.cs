6a831ef85c2d6abcb868957b25e8ceaabd45f6e7
Luca Martinelli <lucxmangajet@gmail.com>
2021-01-03 04:28:39 +0100

    devide miro widgets into trees

--- MiningPrototype/Assets/Scripts/Dialog/Miro/MiroParser.cs
+++ MiningPrototype/Assets/Scripts/Dialog/Miro/MiroParser.cs
@@ -19,14 +19,75 @@ public class MiroParser
         string json = "{\"widgets\":" + textAsset.text + "}";
         var board = JsonUtility.FromJson<MiroBoard>(json);
 
-        for (int i = 0; i < board.widgets.Count; i++)
+        board.FilterOutUnnecessaryWidgets();
+        Debug.Log("Widgets after Filter: " + board.widgets.Count);
+        board.CreateTreeWidgets();
+    }
+}
+
+[System.Serializable]
+public class MiroBoard
+{
+    public List<MiroWidgetRaw> widgets;
+
+    public void CreateTreeWidgets()
+    {
+        Dictionary<string, MiroTreeWidget> treeWidgets = new Dictionary<string, MiroTreeWidget>();
+
+        //create nodes
+        foreach (var w in widgets)
         {
-            MiroWidget current = board.widgets[i];
+            if (w.type == "STICKER")
+            {
+                var nw = new MiroTreeWidget();
+                nw.plainText = w.plainText;
+                treeWidgets.Add(w.id, nw);
+            }
+        }
+
+        //Connect nodes
+        foreach (var w in widgets)
+        {
+            if (w.type == "LINE")
+            {
+                if (treeWidgets.ContainsKey(w.startWidgetId) && treeWidgets.ContainsKey(w.endWidgetId))
+                {
+                    var wStart = treeWidgets[w.startWidgetId];
+                    var wEnd = treeWidgets[w.endWidgetId];
+                    wStart.children.Add(wEnd);
+                    wEnd.children.Add(wStart);
+                }
+            }
+        }
+
+        //Remove all widgets that are not connected by Lines
+        List<string> keysToRemove = new List<string>();
+        foreach (var p in treeWidgets)
+        {
+            if (p.Value.children.Count == 0 && p.Value.parents.Count == 0)
+            {
+                keysToRemove.Add(p.Key);
+            }
+        }
+        foreach (var key in keysToRemove)
+        {
+            treeWidgets.Remove(key);
+        }
+
+        Debug.Log("TreeWidgets: " + treeWidgets.Count);
+    }
+
+
+    public void FilterOutUnnecessaryWidgets()
+    {
+        for (int i = 0; i < widgets.Count; i++)
+        {
+            MiroWidgetRaw current = widgets[i];
             //Remove all but Sticker and Line
             string t = current.type;
             if (t != "STICKER" && t != "LINE")
             {
-                board.widgets.RemoveAt(i);
+                widgets.RemoveAt(i);
                 i--;
             }
             else if (t == "STICKER")
@@ -34,7 +95,7 @@ public class MiroParser
                 //Remove empty stickers
                 if (string.IsNullOrWhiteSpace(current.plainText))
                 {
-                    board.widgets.RemoveAt(i);
+                    widgets.RemoveAt(i);
                     i--;
                 }
             }
@@ -43,23 +104,25 @@ public class MiroParser
                 //Remove lines that are not connected
                 if (string.IsNullOrEmpty(current.endWidgetId) || string.IsNullOrEmpty(current.startWidgetId))
                 {
-                    board.widgets.RemoveAt(i);
+                    widgets.RemoveAt(i);
                     i--;
                 }
             }
         }
-        Debug.Log(board.widgets.Count);
     }
 }
 
-[System.Serializable]
-public class MiroBoard
+
+public class MiroTreeWidget
 {
-    public List<MiroWidget> widgets;
+    public string plainText;
+
+    public List<MiroTreeWidget> parents = new List<MiroTreeWidget>();
+    public List<MiroTreeWidget> children = new List<MiroTreeWidget>();
 }
 
 [System.Serializable]
-public class MiroWidget
+public class MiroWidgetRaw
 {
     public string type;
     public string id;
