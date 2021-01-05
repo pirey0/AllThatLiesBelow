c685e0518fd9c1722dd217aaed9261ab0d89bcca
LucasBoe <lucas@lumotex.de>
2021-01-05 18:46:53 +0100

    added new Dialog Visualization Infrastructure

---
+++ MiningPrototype/Assets/Scripts/UI/DialogElement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
+
+public class DialogElement : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    [SerializeField] VerticalLayoutGroup optionsGroup;
+    [SerializeField] DialogOption dialogOptionPrefab;
+    [SerializeField] ImageSpriteAnimator imageSpriteAnimator;
+    [SerializeField] SpriteAnimation hideAnimation;
+
+    bool isChoice = false;
+
+    public DialogElement Init(string str)
+    {
+        text.text = str;
+        return this;
+    }
+
+    internal DialogElement Init(string[] options, DialogVisualizer dialogVisualizer)
+    {
+        isChoice = true;
+        for (int i = 0; i < options.Length; i++)
+        {
+            InstantiateOption().Init(options[i],i,dialogVisualizer);
+        }
+
+        SetHeight((1f+options.Length)/2);
+
+        return this;
+    }
+
+    private void SetHeight(float height)
+    {
+        (transform as RectTransform).sizeDelta = new Vector2(4, height);
+    }
+
+    private DialogOption InstantiateOption()
+    {
+        return Instantiate(dialogOptionPrefab, optionsGroup.transform);
+    }
+
+    public void Hide()
+    {
+        StopAllCoroutines();
+        StartCoroutine(HideRoutine());
+    }
+
+    public IEnumerator HideRoutine()
+    {
+        text.text = "";
+        imageSpriteAnimator.Play(hideAnimation);
+        while (!imageSpriteAnimator.IsDone())
+            yield return null;
+
+        Destroy(gameObject);
+    }
+}
---
+++ MiningPrototype/Assets/Scripts/UI/DialogOption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogOption : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    DialogVisualizer dialogVisualizer;
+    int index;
+
+    public void Init(string text, int index, DialogVisualizer dialogVisualizer)
+    {
+        this.text.text = text;
+        this.dialogVisualizer = dialogVisualizer;
+        this.index = index;
+    }
+
+    public void Interact()
+    {
+        if (dialogVisualizer != null)
+            dialogVisualizer.SelectOption(index);
+    }
+}
