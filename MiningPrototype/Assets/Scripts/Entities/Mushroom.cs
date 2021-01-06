af83eea1022fdfb21055650dc1d3c44e6e8fc314
LucasBoe <lucas@lumotex.de>
2021-01-06 13:03:36 +0100

    mushroom has particles and sound on jump now

---
+++ MiningPrototype/Assets/Scripts/Entities/Mushroom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : BasicNonPersistantSavable
+{
+    [SerializeField] AudioSource bounceAudio;
+    [SerializeField] ParticleSystem particleSystem;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (bounceAudio.isPlaying)
+            return;
+
+        bounceAudio.pitch = UnityEngine.Random.Range(0.6f, 1);
+        bounceAudio.Play();
+        particleSystem.Play();
+    }
+}
