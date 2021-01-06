c3c41185dcd116242f056cf7a16b36b131f7b643
Luca Martinelli <lucxmangajet@gmail.com>
2021-01-06 21:38:46 +0100

    altar location savable now

--- MiningPrototype/Assets/Scripts/Dialog/AltarLocation.cs
+++ MiningPrototype/Assets/Scripts/Dialog/AltarLocation.cs
@@ -8,12 +8,13 @@ public interface IDialogUser
     void Setup(INodeServiceProvider services, AltarBaseNode node);
 }
 
-public class AltarLocation : PlayerTrigger
+public class AltarLocation : PlayerTrigger, INonPersistantSavable
 {
 
     [SerializeField] bool encounter;
     [NaughtyAttributes.HideIf("encounter")]
     [SerializeField] string dialogName;
+
     [SerializeField] GameObject dialogVisualizerPrefab;
     [SerializeField] GameObject visualAltarPrefab;
     [SerializeField] GameObject altarInventoryPrefab;
@@ -113,6 +114,47 @@ public class AltarLocation : PlayerTrigger
         if (altarInventorySpawnLocation != null)
             UnityEditor.Handles.Label(altarInventorySpawnLocation.position, "I");
     }
+
+    public SpawnableSaveData ToSaveData()
+    {
+        var sd = new AltarLocationSaveData();
+        sd.SaveTransform(transform);
+
+        sd.Encounter = encounter;
+        sd.DialogName = dialogName;
+
+        sd.InventoryLocalPos = new SerializedVector3(altarInventorySpawnLocation.localPosition);
+        sd.VisualLocalPos = new SerializedVector3(visualAltarSpawnLocation.localPosition);
+        sd.VisualizerLocalPos = new SerializedVector3(visualizerSpawnLocation.localPosition);
+
+        return sd;
+    }
+
+    public SaveID GetSavaDataID()
+    {
+        return new SaveID("AltarLocation");
+    }
+
+    public void Load(SpawnableSaveData data)
+    {
+        if(data is AltarLocationSaveData lsd)
+        {
+            encounter = lsd.Encounter;
+            dialogName = lsd.DialogName;
+            altarInventorySpawnLocation.localPosition = lsd.InventoryLocalPos.ToVector3();
+            visualAltarSpawnLocation.localPosition = lsd.VisualLocalPos.ToVector3();
+            visualizerSpawnLocation.localPosition = lsd.VisualizerLocalPos.ToVector3();
+        }
+    }
 #endif
 
+    [System.Serializable]
+    public class AltarLocationSaveData : SpawnableSaveData
+    {
+        public bool Encounter;
+        public string DialogName;
+        public SerializedVector3 VisualizerLocalPos;
+        public SerializedVector3 VisualLocalPos;
+        public SerializedVector3 InventoryLocalPos;
+    }
 }
