088ba7ae6d6a077cccfcb1233e3178e4f2c14c0a
LucasBoe <lucas@lumotex.de>
2021-01-06 11:00:40 +0100

    added new altar chest, when spawned by altar it's not interactalbe though... o.o

---
+++ MiningPrototype/Assets/Scripts/Interaction/AltarChest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarChest : InventoryOwner
+{
+    [SerializeField] SpriteAnimator animator;
+    [SerializeField] SpriteAnimation open, close;
+
+    public override void OpenInventory()
+    {
+        animator.Play(open);
+        base.OpenInventory();
+    }
+
+    public override void CloseInventory()
+    {
+        animator.Play(close);
+        base.CloseInventory();
+    }
+}
