7dc095114477bd9747c6045e98746095fc6c2e5f
LucasBoe <lucas@lumotex.de>
2021-01-07 09:32:49 +0100

    added Minecart with random drops

---
+++ MiningPrototype/Assets/Scripts/Interaction/Minecart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minecart : MirrorWorldFollower, INonPersistantSavable
+{
+    [SerializeField] private InventoryOwner inventoryOwner;
+    public SaveID GetSavaDataID()
+    {
+        return new SaveID("Minecart");
+    }
+    public SpawnableSaveData ToSaveData()
+    {
+        var data = new MinecartSaveData();
+        data.SaveTransform(transform);
+        data.Inventory = inventoryOwner.Inventory;
+        return data;
+    }
+
+    public void Load(SpawnableSaveData dataOr)
+    {
+        if (dataOr is MinecartSaveData data)
+        {
+            inventoryOwner.SetInventory(data.Inventory);
+        }
+    }
+
+    [System.Serializable]
+    public class MinecartSaveData : SpawnableSaveData
+    {
+        public Inventory Inventory;
+    }
+}
---
+++ MiningPrototype/Assets/Scripts/Inventory/InventoryOwnerDropBased.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryOwnerDropBased : InventoryOwner
+{
+    [SerializeField] ItemDrops drops;
+    bool touched = false;
+
+    public override void OpenInventory()
+    {
+        if (!touched)
+        {
+            touched = true;
+
+            if (Inventory.Count <= 0)
+            {
+                Inventory.Add(drops.GetRandomDrop());
+            }
+        }
+        base.OpenInventory();
+    }
+}
