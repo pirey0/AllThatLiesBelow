6ae7d1d6d75c1ad023c37bd1f1f57aa4d1471ba5
LucasBoe <lucas@lumotex.de>
2021-01-07 09:47:36 +0100

    added behaviour for flesh eating plant

--- MiningPrototype/Assets/Scripts/Interaction/FleshEatingPlant.cs
+++ MiningPrototype/Assets/Scripts/Interaction/FleshEatingPlant.cs
@@ -7,9 +7,46 @@ public class FleshEatingPlant : BasicNonPersistantSavable
     [SerializeField] SpriteAnimator spriteAnimator;
     [SerializeField] SpriteAnimation walkOver, snap;
 
+    [SerializeField] float snapDelay, snapAnimationTilSnap, snapAnimationRestDuration;
+
+    [Zenject.Inject] PlayerStateMachine playerStateMachine;
+    [Zenject.Inject] CameraController cameraController;
+
+    bool activated = false;
+    bool inSnapping = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (spriteAnimator.IsDone())
-            spriteAnimator.Play(walkOver);
+        if (!inSnapping)
+        {
+            if (spriteAnimator.IsDone())
+                spriteAnimator.Play(walkOver);
+
+            if (!activated)
+            {
+                activated = true;
+                StartCoroutine(SnappingRoutine());
+            }
+        }
+    }
+
+    IEnumerator SnappingRoutine()
+    {
+        cameraController.Shake(transform.position,shakeType:CameraShakeType.raising,snapDelay,10,1);
+        yield return new WaitForSeconds(snapDelay - snapAnimationTilSnap);
+        spriteAnimator.Play(snap);
+        yield return new WaitForSeconds(snapAnimationTilSnap);
+        inSnapping = true;
+        yield return new WaitForSeconds(snapAnimationRestDuration);
+        inSnapping = false;
+        activated = false;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (inSnapping)
+        {
+            if (collision.gameObject.GetComponent<PlayerInteractionHandler>() != null)
+                playerStateMachine.TakeDamage(DamageStrength.Strong);
+        }
     }
 }
