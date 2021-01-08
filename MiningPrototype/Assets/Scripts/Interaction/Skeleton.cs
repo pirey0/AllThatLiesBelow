ae9676d6dcaa9938722d55fa21a7a374f2e586a0
Luca Martinelli <lucxmangajet@gmail.com>
2021-01-08 21:16:39 +0100

    skeleton now has an inventory

---
+++ MiningPrototype/Assets/Scripts/Interaction/Skeleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : InventoryOwner, INonPersistantSavable
+{
+    public SaveID GetSavaDataID()
+    {
+        return new SaveID(SpawnableIDType.Skeleton);
+    }
+
+    public void Load(SpawnableSaveData data)
+    {
+        if(data is SkeletonSaveData ssd)
+        {
+            SetInventory(ssd.Inventory);
+        }
+    }
+
+    public SpawnableSaveData ToSaveData()
+    {
+        var data = new SkeletonSaveData();
+        data.SaveTransform(transform);
+        data.Inventory = Inventory;
+
+        return data;
+    }
+
+    [System.Serializable]
+    public class SkeletonSaveData : SpawnableSaveData
+    {
+        public Inventory Inventory;
+    }
+}
