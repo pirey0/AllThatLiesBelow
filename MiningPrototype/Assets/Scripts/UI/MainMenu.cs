45bdcb6e7f22f8f7f9eee7117ae7826b9d7ec001
LucasBoe <lucas@lumotex.de>
2021-01-17 19:39:53 +0100

    Added confirm new game when savefile exists

--- MiningPrototype/Assets/Scripts/UI/MainMenu.cs
+++ MiningPrototype/Assets/Scripts/UI/MainMenu.cs
@@ -8,11 +8,11 @@ public class MainMenu : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] SceneReference playScene;
     [SerializeField] UnityEngine.UI.Button loadButton;
-    [SerializeField] Transform targetTransform;
+    [SerializeField] Transform targetTransform, warningMenuTransform;
     [SerializeField] float cameraSpeed, cameraDuration;
     [SerializeField] TMPro.TMP_Text loadText;
     [SerializeField] AnimationCurve introYCameraPan, shortYPan;
-    [SerializeField] AudioSource walkingAudio;
+    [SerializeField] AudioSource walkingAudio, woosh;
 
     [Zenject.Inject] GameInstanceDataManger gameInstanceDataManger;
 
@@ -40,7 +40,7 @@ public class MainMenu : MonoBehaviour
         if (gameInstanceDataManger != null && gameInstanceDataManger.ReturnToMenuFromScene)
         {
             animator.SetTrigger("ReturnToMenu");
-            StartCoroutine(CameraMovement(shortYPan, shortYPan.keys[shortYPan.length-1].time));
+            StartCoroutine(CameraMovement(shortYPan, shortYPan.keys[shortYPan.length - 1].time));
         }
         else
         {
@@ -61,10 +61,37 @@ public class MainMenu : MonoBehaviour
             targetTransform.localPosition = new Vector3(targetTransform.localPosition.x, curve.Evaluate(i), targetTransform.localPosition.z);
 
             if (delayWalkingSounds)
-                walkingAudio.volume = Mathf.Clamp(i - (duration - 3f),0f,1f);
+                walkingAudio.volume = Mathf.Clamp(i - (duration - 3f), 0f, 1f);
 
             yield return null;
         }
+    }
+
+    private IEnumerator New()
+    {
+        if (!SaveHandler.SaveFileExists())
+            yield return Play();
+        else
+        {
+            animator.SetTrigger("ShowWarningMenu");
+
+            if (woosh != null)
+            {
+                woosh.pitch = 1;
+                woosh.Play();
+            }
+        }
+    }
+
+    private IEnumerator Abort()
+    {
+        animator.SetTrigger("HideWarningMenu");
+        if (woosh != null)
+        {
+            woosh.pitch = 0.66f;
+            woosh.Play();
+        }
+        yield return null;
     }
 
     private IEnumerator Play()
