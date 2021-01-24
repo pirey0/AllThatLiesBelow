70d673ee6fd2c6f46bac63412da73e9621126645
Luca Martinelli <lucxmangajet@gmail.com>
2021-01-24 11:57:55 +0100

    fixed item placing previews

---
+++ MiningPrototype/Assets/Scripts/HeldItemPreviewTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeldItemPreviewTester : MonoBehaviour
+{
+    [Zenject.Inject] PlayerInteractionHandler player;
+
+    int i = 0;
+    const int MAX_ITEM_ID = 38;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TryNext();
+        }
+    }
+
+    private void TryNext()
+    {
+        i = (i+1)%MAX_ITEM_ID;
+
+        var info = ItemsData.GetItemInfo((ItemType)i);
+
+        player.SetHeldItemSprite(info);
+    }
+}
